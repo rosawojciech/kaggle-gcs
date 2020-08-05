b840e371d2cc51751ac58c2ca04a03b5f81be8bc
rosawojciech <w.rosa@pollub.pl>
2020-08-05 10:56:35 +0200

    Database update, added kaggle_dataset_name column with username

--- kagglegcs/__init__.py
+++ kagglegcs/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.2'
+last_update = '2020-08-05'
 import csv
 import os
 import pkgutil
@@ -7,8 +7,13 @@ import re
 
 d = os.path.join(os.path.dirname(sys.modules['kagglegcs'].__file__),'data')
 D = {}
+filename = os.listdir( d )[0]
+# obtaining version
+p = re.compile('[a-z_]+([0-9])_([0-9])_([0-9])')
+matchObj = p.match(filename)
+__version__ = matchObj.group(1) + '.' + matchObj.group(2)+'.' +  matchObj.group(3)
 
-with open(os.path.join(d,'kaggle_gcs_paths_0_0_2.csv'), 'r') as csv_file:
+with open(os.path.join(d,filename), 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
         D[row["kaggle_dataset"]] = row['gcs_path'] 
@@ -25,7 +30,7 @@ def get_gcs_path(name):
 
 def gcs_info():
     print('Current kagglegcs version: '+__version__)
-    print('Last datasets update: 2020-08-04')
+    print('Last datasets update: '+ last_update)
     print('Number of available datasets: '+str(len(Dl)))
 
 def gcs_available(pattern = None):
